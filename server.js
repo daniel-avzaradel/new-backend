d355a6051c927cfbaf94a28ebe37e1ab5894a5ea
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-10-17 03:13:05 +0300

    remaking the backend from scratch

--- server.js
+++ server.js
@@ -1,28 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
 const colors = require('colors');
+const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
-const cors = require('cors');
 const uri = process.env.MONGO_URI;
-
-const PORT = process.env.PORT || 5001;
-
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use(express.json());
-
-const usersRouter = require('./routes/users.route');
-app.use('/users', usersRouter);
-
-mongoose.connect(uri);
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB connection was successfully established!'.blue);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`.cyan.underline);
-});
