b1fc752eeb98b10dabbfd2c361a0afb109a6737c
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-10-20 20:08:04 +0300

    created server js and established connection with MongoDB

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const uri = process.env.MONGO_URI;
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log(`MongoDB connection was successfully established`.blue);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`.cyan.underline);
+});
