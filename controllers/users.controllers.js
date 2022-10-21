750d490ac1ebd84d8b48ed73ae4f79edec8b8e2e
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-10-21 07:48:35 +0300

    all routes working

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -8,12 +8,48 @@ const getUsers = asyncHandler((req, res) => {
 });
 
 const getUser = asyncHandler((req, res) => {
-  User.find()
-    .then((users) => res.json(users))
+  const { id } = req.params;
+  User.findById(id)
+    .then((user) => res.json(user))
+    .catch((err) => res.json('Error: ' + err));
+});
+
+const addUser = asyncHandler((req, res) => {
+  const username = req.body.username;
+  const newUser = new User({ username });
+  newUser
+    .save()
+    .then((user) =>
+      res.status(201).json(`Username ${user.username} was saved!`)
+    )
+    .catch((err) => res.json('Error: ' + err));
+});
+
+const updateUser = asyncHandler((req, res) => {
+  const { id } = req.params;
+  const username = req.body.username;
+  const updateUser = User.findById(id);
+
+  updateUser
+    .updateOne({ username })
+    .then((user) => res.json(`username ${username} of id: ${id} was updated`))
+    .catch((err) => res.json('Error: ' + err));
+});
+
+const deleteUser = asyncHandler((req, res) => {
+  const { id } = req.params;
+  const deleteUser = User.findById(id);
+
+  deleteUser
+    .deleteOne()
+    .then((user) => res.json(`${user} was removed`))
     .catch((err) => res.json('Error: ' + err));
 });
 
 module.exports = {
   getUsers,
   getUser,
+  addUser,
+  updateUser,
+  deleteUser,
 };
