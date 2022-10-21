7ec207c05c545a269da9f37c9c7167b9f57b7318
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-10-21 07:22:37 +0300

    created user model

---
+++ models/users.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
